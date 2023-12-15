811cac2a809bb1e297588f2cee0baf8f3fb42e16
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-15 18:44:25 -0300

    Adding the api call in the TaskList component

--- src/components/Main/TaskList.tsx
+++ src/components/Main/TaskList.tsx
@@ -1,13 +1,35 @@
-import { SearchInput } from "./SearchInput"
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 interface TaskListProps {
     actualPage: string | string[],
 }
 
-export function TaskList({actualPage}: TaskListProps) {
+export function TaskList({ actualPage }: TaskListProps) {
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        async function FetchData() {
+            const actualPageData = actualPage.toString().toLowerCase();
+            let response = await fetch(`/api/getTasks?actualPage=${actualPageData}`);
+            
+            if (search !== '') {
+                response = await fetch(`/api/getTasks?actualPage=${actualPageData}&search${search}`);
+            }
+
+            const data = await response.json();
+            console.log(data);
+        }
+
+        FetchData();
+    }, [search])
+
     return (
         <div className="flex flex-col px-16 py-8 w-3/4">
-            <SearchInput />
+            <div className="flex w-full bg-dark-blue p-2 rounded">
+                <Image width={32} height={32} src="/search.svg" alt="" />
+                <input type="text" name="search" id="search" placeholder="Search" className="ml-5 w-full rounded p-1 font-serif text-lg" value={search} onChange={(e) => {setSearch(e.target.value)}}/>
+            </div>
             <h1 className="font-serif text-4xl font-semibold my-5">{actualPage}</h1>
         </div>
     );
