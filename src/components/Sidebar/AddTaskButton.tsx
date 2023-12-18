3d90e2f301c0cd3bdcb4d67fc08f4e7686da8790
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-17 22:55:05 -0300

    Image next component usage

--- src/components/Sidebar/AddTaskButton.tsx
+++ src/components/Sidebar/AddTaskButton.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export function AddTaskButton() {
     return (
         <button className="flex items-center bg-dark-blue rounded p-1 text-white hover:brightness-90">
-            <img className="w-8 mr-4" src="/addTask.svg" alt="Image of a plus sign" />
+            <Image width={40} height={40} className="mr-4" src="/addTask.svg" alt="Image of a plus sign" />
             <p className="font-serif text-xl">Add Task</p>
         </button>
     );
