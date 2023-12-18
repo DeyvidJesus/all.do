87801741d37fdd3d4c27517c037eca8021841aaf
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-17 22:55:16 -0300

    Image next component Usage

--- src/components/Sidebar/ShrinkButton.tsx
+++ src/components/Sidebar/ShrinkButton.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ComponentProps } from "react";
 
 export type ButtonProps = ComponentProps<'button'>;
@@ -5,7 +6,7 @@ export type ButtonProps = ComponentProps<'button'>;
 export function ShrinkButton(props :ButtonProps) {
     return (
         <button className="cursor-pointer" {...props}>
-            <img className="w-10" src="/shrink.svg" alt="Shrink Button" />
+            <Image width={32} height={32} className="w-10" src="/shrink.svg" alt="Shrink Button" />
         </button>
     );
 }
