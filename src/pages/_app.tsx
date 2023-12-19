eb07bb45a625982b3a266911a65b234ca59bc73d
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-19 00:32:20 -0300

    Adding darkModeProvider to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { DarkModeProvider } from '@/context/useDarkMode';
 import '../styles/global.css';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DarkModeProvider>
+      <Component {...pageProps} />
+    </DarkModeProvider>
+  )
 }
