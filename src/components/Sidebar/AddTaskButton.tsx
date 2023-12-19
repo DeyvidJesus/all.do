6060b2cc01642e766675460709ec654a2425c4fd
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-19 00:30:47 -0300

    Updating to NextJs' image component

--- src/components/Sidebar/AddTaskButton.tsx
+++ src/components/Sidebar/AddTaskButton.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export function AddTaskButton() {
     return (
-        <button className="flex items-center bg-dark-blue rounded p-1 text-white hover:brightness-90">
-            <Image width={40} height={40} className="mr-4" src="/addTask.svg" alt="Image of a plus sign" />
+        <button className="flex items-center bg-dark-blue rounded p-1 hover:brightness-90 dark:text-white">
+            <Image width={36} height={36} className="mr-2" src="/addTask.svg" alt="Image of a plus sign" />
             <p className="font-serif text-xl">Add Task</p>
         </button>
     );
--- src/components/Sidebar/FilterItem.tsx
+++ src/components/Sidebar/FilterItem.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 interface FilterItemProps {
@@ -5,7 +6,7 @@ interface FilterItemProps {
     name: string,
 }
 
-export function FilterItem({icon, name}: FilterItemProps) {
+export function FilterItem({ icon, name }: FilterItemProps) {
     let url;
     if (name === "Inbox") {
         url = '/';
@@ -15,9 +16,9 @@ export function FilterItem({icon, name}: FilterItemProps) {
 
     return (
         <li className="border-b-2 rounded border-dark-blue mt-4">
-            <Link className="flex items-center text-white font-serif text-xl cursor-pointer hover:underline hover:bg-dark-blue rounded px-1 py-2" href={url}>
-            <img className="w-8 mr-4" src={icon} alt={`${name} icon`} />
-            {name}
+            <Link className="flex items-center font-serif text-xl cursor-pointer hover:underline hover:bg-dark-blue rounded px-1 py-2" href={url}>
+                <Image width={32} height={32} className="mr-4" src={icon} alt={`${name} icon`} />
+                {name}
             </Link>
         </li>
     );
