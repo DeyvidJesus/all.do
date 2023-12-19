d8223be44af0f635c67b674f0e517b2a1c961423
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-19 00:31:23 -0300

    Styling updates

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,10 @@
 import { TasksMenu } from "./TasksMenu";
 import { Userinfo } from "./Userinfo";
 
-let UserInfo = {
-    username: "Deyvid",
-    userImg: "/userImg.svg",
-}
-
 export function Sidebar() {
     return (
-        <aside className="flex flex-col items-center bg-light-blue w-1/4 h-screen">
-            <Userinfo username={UserInfo.username} userImg={UserInfo.userImg}/>
+        <aside className="flex flex-col items-center bg-light-blue border-r-2 border-dark-blue w-1/4 h-screen font-semibold dark:text-white">
+            <Userinfo />
             <TasksMenu />
         </aside>
     );
