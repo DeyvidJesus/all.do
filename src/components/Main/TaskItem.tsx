857364fb1c91741191353876787675b2617fcda9
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-19 20:55:05 -0300

    Task item component done

--- src/components/Main/TaskItem.tsx
+++ src/components/Main/TaskItem.tsx
@@ -1,21 +1,65 @@
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 interface ItemProps {
+    id: string,
     name: string,
     description: string,
     deadline: string,
-    status: string
+    initialStatus: string
 }
 
-export function TaskItem({ name, description, deadline, status }: ItemProps) {
+export function TaskItem({ id, name, description, deadline, initialStatus }: ItemProps) {
+    const [status, setStatus] = useState(initialStatus);
+    const [checked, setChecked] = useState(status === 'done');
+
+    function handleCheckboxChange() {
+        const newChecked = !checked;
+        setChecked(newChecked);
+
+        const newStatus = newChecked ? 'done' : 'pending';
+        setStatus(newStatus);
+
+        updateBackendStatus(newStatus);
+    };
+
+    async function updateBackendStatus(newStatus: string) {
+        try {
+            await fetch('/api/updateStatus', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ id: id, newStatus }),
+            });
+        } catch (error) {
+            console.error('Error updating status:', error);
+        }
+    }
+
+    function getDayOfWeek(date: string) {
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+        const dayIndex = new Date(date).getDay();
+        return daysOfWeek[dayIndex];
+    }
+
+    const dayOfWeek = getDayOfWeek(deadline);
+
     return (
-        <li className="flex font-serif items-center">
-            {status === 'done' ? <input type="checkbox" name="" id="" className="w-6 h-6 rounded-full appearance-none border-solid border-gray border checked:bg-dark-blue" checked /> : <input type="checkbox" name="" id="" className="w-6 h-6 rounded-full appearance-none border-solid border-gray border checked:bg-dark-blue"/>}
-            <div>
-                <h1>{name}</h1>
-                <p>{description}</p>
+        <li className="flex font-serif items-center mt-6 w-full">
+            <input className="w-8 h-8 rounded-full appearance-none border-solid border-gray border checked:bg-dark-blue cursor-pointer" type="checkbox" checked={checked} onChange={handleCheckboxChange} />
+            {checked && <Image className="absolute ml-0.5 cursor-pointer" src='verified.svg' alt='' height={26} width={26} onClick={handleCheckboxChange} />}
+
+            <div className="ml-4">
+                <h1 className="font-bold text-xl">{name}</h1>
+                <p className="text-lg">{description}</p>
             </div>
-            <h2>{deadline}</h2>
+            <div className="flex flex-col items-center ml-auto">
+                <Image src='calendar.svg' alt='' width={32} height={32} />
+                <h2>{dayOfWeek}</h2>
+            </div>
         </li>
     );
 }
+
