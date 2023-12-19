797be7ef59b8dc5b23df3c061f3c543ab7e96f83
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-19 00:32:05 -0300

    Creating darkMode context

---
+++ src/context/useDarkMode.tsx
@@ -0,0 +1,25 @@
+// contexts/DarkModeContext.js
+
+import { createContext, useState, useContext } from 'react';
+
+const DarkModeContext = createContext();
+
+export const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+    // Save the user's preference to local storage
+    localStorage.setItem('darkMode', !darkMode.toString());
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => {
+  return useContext(DarkModeContext);
+};
