41cf27ad07cf43ca7821d0e81360c253018d4e7d
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-19 20:52:56 -0300

    Update tasks' status route

---
+++ src/pages/api/updateStatus.ts
@@ -0,0 +1,22 @@
+import connect from "@/utils/database";
+import { ObjectId } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+      const { collection } = await connect();
+      
+      const { id, newStatus } = req.body;
+
+      try {
+        await collection.updateOne({ _id: new ObjectId(id) }, { $set: { status: newStatus } });
+  
+        res.status(200).json({ success: true });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+      }
+    } else {
+      res.status(405).json({ success: false, error: 'Method Not Allowed' });
+    }
+  }
