bd0a4328e87145f58152e3e5bb1da7f99b37b977
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 09:58:16 -0300

    Image link adjustment

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -6,7 +6,7 @@ export function DarkModeToggle() {
 
     return (
         <button onClick={() => toggleDarkMode()}>
-            {darkMode ? <Image src='dark-mode-moon.svg' alt='' width={32} height={32}/> : <Image src='light-mode-sun.svg' alt='' width={32} height={32}/>}
+            {darkMode ? <Image src='/dark-mode-moon.svg' alt='' width={32} height={32}/> : <Image src='/light-mode-sun.svg' alt='' width={32} height={32}/>}
         </button>
     )
 }
