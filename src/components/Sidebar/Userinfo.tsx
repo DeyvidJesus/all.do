460f104bcac254f6b68f0faf79e47bc5f46a103a
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 14:07:08 -0300

    Shrink function done

--- src/components/Sidebar/Userinfo.tsx
+++ src/components/Sidebar/Userinfo.tsx
@@ -1,20 +1,25 @@
 import { ShrinkButton } from "./ShrinkButton";
 import { DropdownMenu } from "./DropdownMenu";
 import { DarkModeToggle } from "../DarkModeToggle";
+import { ReactNode } from "react";
 
 let UserInfo = {
     username: "Deyvid",
-    userImg: "/userImg.svg",
 }
 
-export function Userinfo() {
+interface UserInfoProps {
+    children: ReactNode;
+    isShrunk: boolean
+}
+
+export function Userinfo({ children, isShrunk }: UserInfoProps) {
     return (
-        <DropdownMenu username={UserInfo.username} userImg={UserInfo.userImg}>
-            <div>
+        <DropdownMenu isShrunk={isShrunk} username={UserInfo.username}>
+            <div className={`${isShrunk ? 'hidden' : ''}`}>
                 <DarkModeToggle />
             </div>
             <div>
-                <ShrinkButton />
+                {children}
             </div>
         </DropdownMenu>
     );
