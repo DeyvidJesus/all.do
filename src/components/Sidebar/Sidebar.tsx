0579dabef64998bc04bb0faedf7d2dde1ea18148
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 12:19:29 -0300

    Sidebar done

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import { Userinfo } from "./Userinfo";
 
 export function Sidebar() {
     return (
-        <aside className="flex flex-col items-center bg-light-blue border-r-2 border-dark-blue w-1/4 h-screen font-semibold dark:text-white">
+        <aside className="flex flex-col items-center bg-white border-r-4 border-dark-blue w-1/4 h-screen font-semibold dark:text-white dark:bg-light-blue dark:border-white">
             <Userinfo />
             <TasksMenu />
         </aside>
