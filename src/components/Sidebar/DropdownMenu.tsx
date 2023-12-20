c23cb7dc7d35d0ccbab3aa14d5130050727c0007
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 12:18:35 -0300

    Dropdown menu style done

--- src/components/Sidebar/DropdownMenu.tsx
+++ src/components/Sidebar/DropdownMenu.tsx
@@ -4,14 +4,18 @@ import { ReactNode, useEffect, useRef, useState } from "react";
 
 interface UserInfo {
     username: string,
-    userImg: string,
     children: ReactNode
 }
 
-export function DropdownMenu({ username, userImg, children }: UserInfo) {
+export function DropdownMenu({ username, children }: UserInfo) {
     const { darkMode } = useDarkMode();
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const menuRef = useRef<HTMLDivElement>(null);
+
+    let srcObj = {
+        userImgSrc: darkMode == true ? '/userImg.svg' : '/userImgDark.svg',
+        downArrowSrc: darkMode == true ? '/downArrow.svg' : '/downArrowDark.svg',
+    }
 
     useEffect(() => {
         let handler = (e: MouseEvent) => {
@@ -29,14 +33,14 @@ export function DropdownMenu({ username, userImg, children }: UserInfo) {
 
     return (
         <div ref={menuRef} className="flex flex-row w-full justify-between items-center px-5 py-3">
-            <div onClick={() => { setIsDropdownOpen(!isDropdownOpen); }} className="cursor-pointer py-2 px-2 rounded hover:bg-dark-blue">
+            <div onClick={() => { setIsDropdownOpen(!isDropdownOpen); }} className="cursor-pointer py-2 px-2 rounded hover:bg-royal-blue">
                 <div className="flex items-end justify-between w-auto cursor-pointer">
-                    <Image width={40} height={40} src={userImg} alt="Image to represent user's profile" />
+                    <Image width={40} height={40} src={srcObj.userImgSrc} alt="Image to represent user's profile" />
                     <h2 className="ml-3 text-3xl font-serif font-semibold dark:text-white">{username}</h2>
-                    <Image width={32} height={32} src="/downArrow.svg" alt="Down arrow to indicate there's a menu" />
+                    <Image width={32} height={32} src={srcObj.downArrowSrc} alt="Down arrow to indicate there's a menu" />
                 </div>
             </div><div
-                className={`absolute w-48 rounded top-20 left-6 bg-dark-blue text-white p-2 dropdown-menu ${isDropdownOpen ? 'active' : 'inactive'}`}>
+                className={`absolute w-48 rounded top-20 left-6 bg-royal-blue text-white p-2 dropdown-menu ${isDropdownOpen ? 'active' : 'inactive'}`}>
                 <ul>
                     <li>
                         <a className="flex items-center hover:underline" href="">
