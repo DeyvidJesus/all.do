f620d1b488fe1c3097aba920156db720e9e80d4a
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 12:18:46 -0300

    filter item done

--- src/components/Sidebar/FilterItem.tsx
+++ src/components/Sidebar/FilterItem.tsx
@@ -15,8 +15,8 @@ export function FilterItem({ icon, name }: FilterItemProps) {
     }
 
     return (
-        <li className="border-b-2 rounded border-dark-blue mt-4">
-            <Link className="flex items-center font-serif text-xl cursor-pointer hover:underline hover:bg-dark-blue rounded px-1 py-2" href={url}>
+        <li className="border-b-2 rounded border-royal-blue mt-4 dark:border-white">
+            <Link className="flex items-center font-serif text-xl cursor-pointer hover:underline hover:bg-royal-blue rounded px-1 py-2" href={url}>
                 <Image width={32} height={32} className="mr-4" src={icon} alt={`${name} icon`} />
                 {name}
             </Link>
