a7fb5eea3f42a2c0f9fda55e05bb03ed8267a7e9
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 14:06:48 -0300

    Handle shrink done

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+import { ShrinkButton } from "./ShrinkButton";
 import { TasksMenu } from "./TasksMenu";
 import { Userinfo } from "./Userinfo";
 
 export function Sidebar() {
+    const [isShrunk, setIsShrunk] = useState(false);
+
+    const handleShrinkToggle = () => {
+      setIsShrunk(!isShrunk);
+    };
+
     return (
-        <aside className="flex flex-col items-center bg-white border-r-4 border-dark-blue w-1/4 h-screen font-semibold dark:text-white dark:bg-light-blue dark:border-white">
-            <Userinfo />
-            <TasksMenu />
+        <aside className={`flex flex-col items-center bg-white border-r-4 border-dark-blue w-1/3 h-screen font-semibold dark:text-white dark:bg-light-blue dark:border-white ${isShrunk ? 'w-28' : ''} transition-all ease-in-out delay-100`}>
+            <Userinfo isShrunk={isShrunk}>
+                <ShrinkButton onClick={handleShrinkToggle} />
+            </Userinfo>
+            <TasksMenu isShrunk={isShrunk}/>
         </aside>
     );
 }
