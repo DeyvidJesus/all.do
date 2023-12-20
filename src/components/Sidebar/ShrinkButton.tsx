11bd609df9f6376cc1b806ff5864f0a4a7b7cd06
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 12:19:11 -0300

    Shrink button style done

--- src/components/Sidebar/ShrinkButton.tsx
+++ src/components/Sidebar/ShrinkButton.tsx
@@ -1,12 +1,17 @@
+import { useDarkMode } from "@/context/useDarkMode";
 import Image from "next/image";
 import { ComponentProps } from "react";
 
 export type ButtonProps = ComponentProps<'button'>;
 
 export function ShrinkButton(props :ButtonProps) {
+    const { darkMode } = useDarkMode();
+
+    let shrinkSrc = darkMode == true ? '/shrink.svg' : '/shrinkDark.svg';
+
     return (
         <button className="cursor-pointer" {...props}>
-            <Image width={32} height={32} className="w-10" src="/shrink.svg" alt="Shrink Button" />
+            <Image width={32} height={32} className="w-10" src={shrinkSrc} alt="Shrink Button" />
         </button>
     );
 }
