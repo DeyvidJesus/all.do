8ae845693b3aeeab76bf37c64f63e4066b2e89f1
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 09:59:46 -0300

    New structure o getTasks route
    Now there's a switch case to the actualPage and it changes the query according to the needs

--- src/pages/api/getTasks.ts
+++ src/pages/api/getTasks.ts
@@ -7,13 +7,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { actualPage, search } = req.query;
 
     try {
-        if (search !== undefined) {
-            const tasksData = await collection.find({project: actualPage, name: search}).toArray();
-            res.status(200).json(tasksData);
-        } else {
-            const tasksData = await collection.find({project: actualPage}).toArray();
-            res.status(200).json(tasksData);
+        let query;
+
+        switch (actualPage) {
+            case 'today':
+                const todayDateString = new Date().toLocaleDateString('en-US');
+                query = { deadline: todayDateString }
+                break;
+            case 'upcoming':
+                const today = new Date().toLocaleDateString('en-US');
+
+                query = {
+                    deadline: { $gte: today },
+                };
+                break;
+            default:
+                query = { project: actualPage }
+                break;
         }
+
+        const tasksData = await collection.find(query).toArray();
+        res.status(200).json(tasksData);
     } catch (err) {
         console.log(err)
         res.status(500).json({ error: 'Internal Server Error' });
