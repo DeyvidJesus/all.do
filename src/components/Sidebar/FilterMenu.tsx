bf24897e6086d733ca57a3d01eb50cb3c8870fc2
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 12:18:56 -0300

    filter menu done

--- src/components/Sidebar/FilterMenu.tsx
+++ src/components/Sidebar/FilterMenu.tsx
@@ -1,12 +1,22 @@
+import { useDarkMode } from "@/context/useDarkMode";
 import { FilterItem } from "./FilterItem";
 
 export function FilterMenu() {
+    const { darkMode } = useDarkMode();
+
+    let srcObj = {
+        inboxSrc: darkMode == true ? '/inbox.svg' : '/inboxDark.svg',
+        todaySrc: darkMode == true ? '/today.svg' : '/todayDark.svg',
+        upcomingSrc: darkMode == true ? '/upcoming.svg' : '/upcomingDark.svg',
+        allSrc: darkMode == true ? '/allTasks.svg' : '/allTasksDark.svg',
+    }
+
     return (
         <ul>
-            <FilterItem icon="/inbox.svg" name="Inbox" />
-            <FilterItem icon="/today.svg" name="Today" />
-            <FilterItem icon="/upcoming.svg" name="Upcoming" />
-            <FilterItem icon="/allTasks.svg" name="All" />
+            <FilterItem icon={srcObj.inboxSrc} name="Inbox" />
+            <FilterItem icon={srcObj.todaySrc} name="Today" />
+            <FilterItem icon={srcObj.upcomingSrc} name="Upcoming" />
+            <FilterItem icon={srcObj.allSrc} name="All" />
         </ul>
     );
 }
