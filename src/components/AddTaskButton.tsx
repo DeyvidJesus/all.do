303aa856c804aac7255cc0ae0e5e4b7fb41f2919
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-20 11:55:38 -0300

    Add task button style done

--- src/components/AddTaskButton.tsx
+++ src/components/AddTaskButton.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function AddTaskButton() {
     return (
-        <button className="flex items-center bg-dark-blue rounded p-1 hover:brightness-90 dark:text-white">
+        <button className="flex items-center bg-royal-blue rounded p-1 hover:brightness-90 text-white dark:bg-royal-blue">
             <Image width={36} height={36} className="mr-2" src="/addTask.svg" alt="Image of a plus sign" />
             <p className="font-serif text-xl">Add Task</p>
         </button>
