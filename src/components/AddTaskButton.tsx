94ed19943c8c4c38fc6c7fbc19a304204bbd5334
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-22 12:58:00 -0300

    Styling fixes

--- src/components/AddTaskButton.tsx
+++ src/components/AddTaskButton.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export function AddTaskButton() {
     return (
-        <button className="flex items-center bg-royal-blue rounded p-1 hover:brightness-90 text-white dark:bg-royal-blue">
-            <Image width={36} height={36} className="mr-2 sm:w-7 sm:h-7 lg:w-9 lg:h-9" src="/addTask.svg" alt="Image of a plus sign" />
+        <button className="flex items-center bg-royal-blue rounded p-1 hover:brightness-90 text-white">
+            <Image width={36} height={36} className="mr-2 sm:w-7 sm:h-7 lg:w-8 lg:h-8" src="/addTask.svg" alt="Image of a plus sign" />
             <p className="font-serif sm:text-lg lg:text-xl">Add Task</p>
         </button>
     );
