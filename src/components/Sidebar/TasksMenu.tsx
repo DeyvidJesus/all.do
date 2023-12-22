81633b9107989ad7370e235a477fee5cbd6ca059
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-22 12:58:35 -0300

    Rendering add project button

--- src/components/Sidebar/TasksMenu.tsx
+++ src/components/Sidebar/TasksMenu.tsx
@@ -1,4 +1,5 @@
 import { AddTaskButton } from "../AddTaskButton";
+import { AddProjectButton } from "./AddProjectButton";
 import { FilterMenu } from "./FilterMenu";
 
 interface TasksMenuProps {
@@ -9,6 +10,7 @@ export function TasksMenu({isShrunk}: TasksMenuProps) {
     return (
         <div className={`flex flex-col ${isShrunk ? 'hidden' : 'sm:w-11/12 lg:w-10/12'} mb-10`}>
             <AddTaskButton />
+            <AddProjectButton />
             <FilterMenu />
         </div>
     );
