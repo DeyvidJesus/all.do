60e2b7faa6a4e28c621a8eb4b28bf8140c64cbd4
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-12-29 14:59:26 -0300

    Organizing folders

--- src/components/Utils/AddTaskButton.tsx
+++ src/components/Utils/AddTaskButton.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import { ComponentProps } from "react";
 
-export function AddTaskButton() {
+type ButtonProps = ComponentProps<'button'>;
+
+export function AddTaskButton(props: ButtonProps) {
     return (
-        <button className="flex items-center bg-royal-blue rounded p-1 hover:brightness-90 text-white">
+        <button className="flex items-center bg-royal-blue rounded p-1 hover:brightness-90 text-white" {...props}>
             <Image width={36} height={36} className="mr-2 sm:w-7 sm:h-7 lg:w-8 lg:h-8" src="/addTask.svg" alt="Image of a plus sign" />
             <p className="font-serif sm:text-lg lg:text-xl">Add Task</p>
         </button>
