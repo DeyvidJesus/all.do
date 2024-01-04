898e1f2cbcbff512aac589971b2e68f7aac31fe4
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-04 20:15:04 -0300

    Changing document's folder

--- src/components/Sidebar/Project/AddProjectButton.tsx
+++ src/components/Sidebar/Project/AddProjectButton.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import { ComponentProps } from "react";
 
-export function AddProjectButton() {
+type ButtonProps = ComponentProps<'button'>;
+
+export function AddProjectButton(props: ButtonProps) {
     return (
-        <button className="flex items-center bg-gray rounded p-1 hover:brightness-90 text-white mt-2">
+        <button className="flex items-center bg-gray rounded p-1 hover:brightness-90 text-white mt-2" {...props}>
             <Image width={36} height={36} className="mr-2 sm:w-6 sm:h-6 lg:w-8 lg:h-8" src="/addTask.svg" alt="Image of a plus sign" />
             <p className="font-serif sm:text-lg lg:text-xl">Add Project</p>
         </button>
