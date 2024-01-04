014883faf7fbf3ffc4aeae04f29d3e698a6445d0
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-04 20:16:47 -0300

    Delete projects button structure

---
+++ src/components/Sidebar/Project/DeleteProjectButton.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image"
+import { useState } from "react";
+import { ConfirmationPopup } from "../../Main/ConfirmationPopup";
+
+interface DeleteProjectProps {
+    id: string | undefined,
+}
+
+export function DeleteProjectButton({ id }: DeleteProjectProps) {
+    const [isConfirmationVisible, setConfirmationVisible] = useState(false);
+
+    const handleDeleteTask = () => {
+        setConfirmationVisible(true);
+    };
+
+    async function handleConfirmDelete() {
+        try {
+            await fetch('/api/projects/deleteProject', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ id }),
+            });
+
+            window.location.reload();
+        } catch (error) {
+            console.error('Error updating status:', error);
+        } finally {
+            setConfirmationVisible(false);
+        }
+    }
+
+    const handleCancelDelete = () => {
+        setConfirmationVisible(false);
+    };
+
+    return (
+        <div className="self-center">
+            <button className="flex flex-col items-center text-red ml-4 hover:brightness-75" onClick={handleDeleteTask}>
+                <Image src={'/trash.svg'} width={24} height={24} alt='' />
+                <h2 className={`sm:text-sm md:text-lg`}>Delete</h2>
+            </button>
+
+            {isConfirmationVisible && (
+                <ConfirmationPopup
+                    message="Are you sure you want to delete this project?"
+                    onConfirm={handleConfirmDelete}
+                    onCancel={handleCancelDelete}
+                />
+            )}
+        </div>
+    );
+}
