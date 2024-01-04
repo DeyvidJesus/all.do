c33951fc8a402d3121159decfefe34dd05fa3e14
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-04 20:19:10 -0300

    simple udpate

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -14,7 +14,6 @@ export default async function connect() {
     await client.connect();
 
     const db = client.db("alldo");
-    const collection = db.collection("tasks");
 
-    return {db, collection, client};
+    return {db, client};
 }
