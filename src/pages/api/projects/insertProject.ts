aeed0e418b5ec1b3a63d6405634ecbe10d9ac378
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-04 20:17:50 -0300

    insert projects route

---
+++ src/pages/api/projects/insertProject.ts
@@ -0,0 +1,38 @@
+import connect from "@/utils/database";
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface FormDataProps {
+  formData: {
+    name: string,
+    color: string,
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { db } = await connect();
+    const collection = db.collection("projects");
+
+    const { formData }: FormDataProps = req.body;
+
+    const { name, color } = formData;
+
+    if (name == null || color == null) {
+      res.status(400).json({ success: false, error: "The request is malformed or contains invalid parameters" });
+      return;
+    }
+
+    const newDate = new Date().toLocaleDateString('en-US');
+
+    try {
+      collection.insertOne({ name, created_at: newDate, color});
+
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ success: false, error: 'Method Not Allowed' });
+  }
+}
