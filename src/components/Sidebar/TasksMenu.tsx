fb65787c80a11ff1b73d16f5c640ffeeae173488
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-04 20:17:19 -0300

    Rendering the add project form

--- src/components/Sidebar/TasksMenu.tsx
+++ src/components/Sidebar/TasksMenu.tsx
@@ -1,15 +1,33 @@
-import { AddProjectButton } from "./AddProjectButton";
+import { useState } from "react";
+import { AddProjectButton } from "./Project/AddProjectButton";
 import { FilterMenu } from "./FilterMenu";
+import { AddProjectForm } from "./Project/AddProjectForm";
 
 interface TasksMenuProps {
     isShrunk: boolean,
 }
 
-export function TasksMenu({isShrunk}: TasksMenuProps) {
+export function TasksMenu({ isShrunk }: TasksMenuProps) {
+    const [isAddProjectFormVisible, setIsAddProjectFormVisible] = useState(false);
+
+    function closeModal(e: React.SyntheticEvent) {
+        if (e.target === e.currentTarget) {
+            setIsAddProjectFormVisible(false);
+        }
+    }
+
     return (
-        <div className={`flex flex-col ${isShrunk ? 'hidden' : 'sm:w-11/12 lg:w-10/12'} mb-10`}>
-            <AddProjectButton />
-            <FilterMenu />
-        </div>
+        <>
+            <div className={`flex flex-col ${isShrunk ? 'hidden' : 'sm:w-11/12 lg:w-10/12'} mb-10`}>
+                <AddProjectButton onClick={() => setIsAddProjectFormVisible(!isAddProjectFormVisible)} />
+                <FilterMenu />
+            </div>
+
+            {isAddProjectFormVisible &&
+                <div className="absolute w-screen top-0 left-0 h-screen flex justify-center items-center bg-transparent-gray" onClick={closeModal}>
+                    <AddProjectForm closeModal={closeModal} />
+                </div>
+            }
+        </>
     );
 }
