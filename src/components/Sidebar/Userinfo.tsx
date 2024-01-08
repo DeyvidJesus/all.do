b50e657f56ed49c8b8cd378c22d10ad4b2a1cf6f
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-08 15:38:49 -0300

    Limiting the username to 10 letters

--- src/components/Sidebar/Userinfo.tsx
+++ src/components/Sidebar/Userinfo.tsx
@@ -1,9 +1,6 @@
+import { useSession } from "next-auth/react";
 import { DropdownMenu } from "./DropdownMenu";
 import { ReactNode } from "react";
-
-let UserInfo = {
-    username: "Deyvid",
-}
 
 interface UserInfoProps {
     children: ReactNode;
@@ -11,8 +8,15 @@ interface UserInfoProps {
 }
 
 export function Userinfo({ children, isShrunk }: UserInfoProps) {
+    const { data: session } = useSession();
+    let name = session?.user?.name || '';
+
+    if (name.length > 10) {
+        name = name.substring(0, 10) + '...';
+    }
+
     return (
-        <DropdownMenu isShrunk={isShrunk} username={UserInfo.username}>
+        <DropdownMenu isShrunk={isShrunk} username={name}>
             <div>
                 {children}
             </div>
