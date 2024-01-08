e49274031463135cf1f68673ff945a751852e3ff
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-08 15:39:53 -0300

    Deleting projects of the logged in user

--- src/pages/api/projects/deleteProject.ts
+++ src/pages/api/projects/deleteProject.ts
@@ -5,23 +5,33 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'DELETE') {
         const { db } = await connect();
-        const collection = db.collection("projects");
+        const projectsCollection = db.collection("projects");
+        const tasksCollection = db.collection("tasks");
 
-        const { id } = req.body;
+        const { id, nameToLowerCase, user_email } = req.body;
 
         try {
-            const result = await collection.deleteOne({ _id: new ObjectId(id) });
+            await tasksCollection.deleteMany({ project: nameToLowerCase, user_email });
 
-            if (result.deletedCount === 1) {
-                res.status(200).json({ success: true });
-            } else {
-                res.status(404).json({ success: false, error: "Resource not found" });
-            }
+            res.status(200).json({ success: true });
         } catch (error) {
-            console.error(error);
-            res.status(500).json({ success: false, error: 'Internal Server Error' });
-        }
-    } else {
-        res.status(405).json({ success: false, error: 'Method Not Allowed' });
+        console.error(error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+        return;
     }
+
+    try {
+        const result = await projectsCollection.deleteOne({ _id: new ObjectId(id) });
+        if (result.deletedCount === 1) {
+            res.status(200).json({ success: true });
+        } else {
+            res.status(404).json({ success: false, error: "Resource not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+} else {
+    res.status(405).json({ success: false, error: 'Method Not Allowed' });
+}
 }
