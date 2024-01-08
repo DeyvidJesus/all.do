c6dee98178295e7587fccec0da3cc4eca166ccea
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-08 15:39:36 -0300

    Changing to github provider and saving user data

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,36 @@
-// pages/api/auth/[...nextauth].ts
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
 import FacebookProvider from 'next-auth/providers/facebook';
+import connect from '@/utils/database';
 
-export default NextAuth({
+const options = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID || '',
-      clientSecret: process.env.GOOGLE_SECRET || '',
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     FacebookProvider({
-        clientId: process.env.FACEBOOK_ID || '',
-        clientSecret: process.env.FACEBOOK_SECRET || ''
-    })
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
   ],
-  pages: {
-    signIn: '/tasks/Inbox', // Customize the sign-in page URL
-  },
-});
+  callbacks: {
+    async signIn({ user }: any) {
+      const { db } = await connect();
+
+      const existingUser = await db.collection('users').findOne({ email: user.email });
+
+      if (!existingUser) {
+        await db.collection('users').insertOne({
+          name: user.name,
+          email: user.email,
+          password: ""
+        });
+      }
+
+      return true;
+    },
+  }
+};
+
+export default NextAuth(options);
