10e2042b59662ab7fe74d83a086521f0b5a572bf
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-11 11:53:01 -0300

    Responsive layout

--- src/components/Auth/AuthLayout.tsx
+++ src/components/Auth/AuthLayout.tsx
@@ -8,10 +8,10 @@ interface AuthLayoutProps {
 export function AuthLayout({ children }: AuthLayoutProps) {
     return (
         <div className="flex h-screen">
-            <div className="w-1/2 font-serif flex justify-center items-center flex-col">
+            <div className="lg:w-1/2 font-serif flex justify-center items-center flex-col w-screen">
                 {children}
             </div>
-            <div className="w-1/2 bg-royal-blue flex flex-col items-center justify-evenly py-4">
+            <div className="lg:visible lg:flex w-1/2 bg-royal-blue flex-col items-center justify-evenly py-4 hidden invisible">
                 <Image src="/authImage.svg" alt="" width={450} height={450} />
                 <div className="flex flex-col items-center text-white font-serif text-2xl text-center">
                     <h2 className="font-bold">Efficiency Unleashed: <br /> Streamline Your Day with All.do</h2>
