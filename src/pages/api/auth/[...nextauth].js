58465f91ce6478e70866dfb9552892e47fbdf8f7
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-14 18:21:52 -0300

    Importing connect

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
+import connect from "@/utils/database"
 
 export const authOptions = {
   providers: [  
