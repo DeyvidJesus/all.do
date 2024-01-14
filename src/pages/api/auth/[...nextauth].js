face3b123e51ff1672b32c3b7796d9170b4c8739
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-14 18:19:09 -0300

    Adding signIn callback

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -15,6 +15,21 @@ export const authOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+    async signIn({ user }) {
+      const { db } = await connect();
+
+      const existingUser = await db.collection('users').findOne({ email: user.email });
+
+      if (!existingUser) {
+        await db.collection('users').insertOne({
+          name: user.name,
+          email: user.email,
+          password: "",
+        });
+      }
+
+      return true;
+    },
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
       if (account) {
