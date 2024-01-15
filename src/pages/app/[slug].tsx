3731ec7b04a9fd1bdffb0782c0165335346f5a05
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-15 01:30:17 -0300

    ...

--- src/pages/app/[slug].tsx
+++ src/pages/app/[slug].tsx
@@ -24,7 +24,7 @@ function Page() {
                 setTimeout(checkSessionStatus, 500);
             } else {
                 // User is authenticated
-                console.log({session});
+                console.log(session?.name);
                 setLoading(false);
             }
         };
